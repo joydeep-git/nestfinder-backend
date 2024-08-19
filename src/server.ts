acdfd9240a732b7351ed2079ac2a8c710598db96
Joydeep <joydeep.accounts@tuta.io>
2024-08-19 11:14:54 +0530

    connected mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-const port: number = parseInt(process.env.PORT || "3001", 10);
+const mongoDBString: string = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.CLUSTER_NAME}.zcmkg.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.CLUSTER_NAME}`;
+
+mongoose
+  .connect(mongoDBString)
+  .then(() => {
+    console.log(`MongoDB Connected. Cluster -> ${process.env.CLUSTER_NAME}`);
+  })
+  .catch((err) => console.log(`MongoDB ${process.env.CLUSTER_NAME}: ${err}`));
 
 const app: Application = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
-const msg: string = "Hello Node!";
-
-app.listen(port, () => {
-  console.log(msg);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on PORT: ${process.env.PORT}`);
 });
-
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json({Message: msg})
-})
