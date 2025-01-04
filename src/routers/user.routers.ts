bca7f4705eb0b280978c8e1734ff2cf5aaa79c9f
joydeep-digitalwoods <joydeep@digitalwoods.net>
2025-01-04 14:53:47 +0530

    user router

--- src/routers/user.routers.ts
+++ src/routers/user.routers.ts
@@ -1,8 +1,12 @@
 import express, {Router} from "express";
+import { updateUserController } from "../controllers/user.controller.ts";
 
 
 const userRouter: Router = express.Router();
 
+
+
+userRouter.post("/update", updateUserController);
 
 
 
