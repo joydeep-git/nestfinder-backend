34f1131f731e185c87a326e511d8b78e84b94ac9
joydeep-git <joydeep.accounts@tuta.io>
2025-01-04 21:39:56 +0530

    update user setup

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,2 +1,15 @@
-// import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { mongooseErrorHandler } from "../utils/mongooseErrorHandler.ts";
+import ErrorHandler from "../utils/ErrorHandler.ts";
 
+export const updateUserController = async (req: Request, res: Response, next: NextFunction) => {
+
+  // if (req?.user?.id !== req.params.id) return next(new ErrorHandler({ status: 401, message: "Unauthenticated!" }));
+
+  try {
+
+  } catch (err) {
+    return next(mongooseErrorHandler(err));
+  }
+
+};
