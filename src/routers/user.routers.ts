e2663ab1d36560bf5830e8185ff418b32e867935
joydeep-git <joydeep.accounts@tuta.io>
2025-01-26 04:04:29 +0530

    refined userRouter

--- src/routers/user.routers.ts
+++ src/routers/user.routers.ts
@@ -1,15 +1,22 @@
 import express, {Router} from "express";
-import {deleteUserController, getAllUserProducts, updateUserController} from "../controllers/user.controller.ts";
+import {
+    deleteUserController,
+    getAllUserProducts, updateProfilePicture,
+    updateUserController
+} from "../controllers/user.controller.ts";
+import {verifyToken} from "../controllers/auth.controller.ts";
 
 
 const userRouter: Router = express.Router();
 
 
-userRouter.post("/update-details/:id", updateUserController);
+userRouter.post("/update-details/:id", verifyToken(true), updateUserController);
 
-userRouter.delete("/delete-user/:id", deleteUserController);
+userRouter.post("/update-picture/:id", verifyToken(true), updateProfilePicture);
 
-userRouter.get("/user-products/:id", getAllUserProducts);
+userRouter.delete("/delete-user/:id", verifyToken(true), deleteUserController);
+
+userRouter.get("/user-products/:id", verifyToken(true), getAllUserProducts);
 
 
 
