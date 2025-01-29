c3f2e33004c50c9cbc7ceba53a5775af501688db
joydeep-git <joydeep.accounts@tuta.io>
2025-01-29 18:44:05 +0530

    added product router

--- src/routers/product.routers.ts
+++ src/routers/product.routers.ts
@@ -1,10 +1,18 @@
-import express, {Router} from "express";
-import {createProduct} from "../controllers/product.controller.ts";
+import express, { Router } from "express";
+import { createProduct, deleteProduct, getProductDetails, updateProductDetails } from "../controllers/product.controller.ts";
+import { verifyToken } from "../controllers/auth.controller.ts";
+
 
 
 const productRouter: Router = express.Router();
 
-productRouter.post("/create", createProduct)
+productRouter.post("/create/:id", verifyToken(true) , createProduct);
+
+productRouter.get("/product-details/:id/:productId", getProductDetails);
+
+productRouter.post("/update-product/:id/:productId", updateProductDetails);
+
+productRouter.delete("/delete-product/:id/:productId", verifyToken(true), deleteProduct);
 
 
 
