bebdddebea3fa9cfbd81337750788fb8d7d75c15
joydeep-git <joydeep.accounts@tuta.io>
2025-02-06 01:12:13 +0530

    added validator

--- src/routers/product.routers.ts
+++ src/routers/product.routers.ts
@@ -1,16 +1,18 @@
 import express, { Router } from "express";
 import { createProduct, deleteProduct, getProductDetails, updateProductDetails } from "../controllers/product.controller.ts";
 import { verifyToken } from "../controllers/auth.controller.ts";
+import validProductChecker from "../middlewares/ValidProductChecker.ts";
+
 
 
 
 const productRouter: Router = express.Router();
 
-productRouter.post("/create/:id", verifyToken(true) , createProduct);
+productRouter.post("/create/:id", verifyToken(true), validProductChecker, createProduct );
 
 productRouter.get("/get-product-details/:id/:productId", getProductDetails);
 
-productRouter.post("/update-product/:id/:productId", updateProductDetails);
+productRouter.post("/update-product/:id/:productId", verifyToken(true), validProductChecker, updateProductDetails);
 
 productRouter.delete("/delete-product/:id/:productId", verifyToken(true), deleteProduct);
 
