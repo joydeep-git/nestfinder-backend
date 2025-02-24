c8a00158e83efec37d39c5dbd6b7aab10d9681c0
joydeep-git <joydeep.accounts@tuta.io>
2025-02-24 09:49:51 +0530

    changed server.ts to class based

--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,115 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
-import mongoose, {Error as mongooseError} from "mongoose";
-import cookieParser from "cookie-parser";
-import authRouter from "./routers/auth.routers";
-import userRouter from "./routers/user.routers.ts";
 import cors from "cors";
-import productRouter from "./routers/product.routers.ts";
-import errorMiddleware from "./middlewares/errorMiddleware.ts";
-import {verifyToken} from "./controllers/auth.controller.ts";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import authRouter from "./routers/authRouter";
+import userRouter from "./routers/userRouter";
+import productRouter from "./routers/productRouter";
+import errorMiddleware from "./middlewares/errorMiddleware";
 
 
-// config DOTENV
-dotenv.config();
+class Server {
+
+	protected app: Application;
+
+	protected port: number;
 
 
+	constructor() {
 
-// connect mongoDB
-const mongodbStr: string = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.giap0.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGODB_CLUSTER}`;
-
-
-
-// Connect MongoDB with my project
-mongoose.connect(mongodbStr).then((): void => {
-  console.log("Database Status: LIVE");
-}).catch((err: mongooseError): void => {
-  console.log(err);
-  process.exit(1);
-});
+		this.dotenv(); // load env
 
 
+		this.app = express();
+		this.port = parseInt(process.env.PORT || "5001", 10);
 
-// default PORT + Fallback
-const port: number = parseInt(process.env.PORT || "5001", 10);
+		this.run();
 
-
-
-// init APP
-const app: Application = express();
+	}
 
 
+	// calling all methods in async fn
+	private async run() {
 
-// Use Cors
-app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000", // frontend URL
-    credentials: true, // Allow cookies
-  }));
+		try {
+			await this.connectDb();
+			this.useMiddleWares();
+			this.createRoutes();
+			this.useErrorMiddleware();
+			this.startServer();
+		} catch (err) {
+			console.error("failed to start server :", err);
+			process.exit(1);
+		}
 
-
-
-// Body parser
-app.use(express.json());
-
-
-
-// cookie-parser
-app.use(cookieParser());
-
-
-// router defined
-app.use("/api/auth", authRouter);
-
-app.use("/api/user", userRouter);
-
-app.use("/api/product", productRouter);
+	}
 
 
 
-// Handle Error using Middleware
-app.use(errorMiddleware);
+	// env variables config
+	private dotenv() {
+		dotenv.config();
+	}
+
+
+	// Connect to mongoDB
+	private async connectDb() {
+		try {
+			if (!process.env.MONGODB_USERNAME || !process.env.MONGODB_PASSWORD || !process.env.MONGODB_CLUSTER) {
+				throw new Error("DB variables are missing!");
+			}
+
+			const mongodbStr = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.giap0.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGODB_CLUSTER}`;
+
+			await mongoose.connect(mongodbStr);
+			console.log("Database Status: LIVE");
+
+		} catch (err) {
+			console.error("MongoDB Connection Error:", err);
+			throw err;
+		}
+	}
 
 
 
-// PORT defined
-app.listen(port, (): void => {
-  console.log("CORS Server PORT:", port);
-});
+	// use middlewares for CORS, handle errors, request body parser, cookies
+	private useMiddleWares() {
+
+		this.app.use(cors({
+			origin: process.env.CLIENT_URL || "http://localhost:3000", // frontend URL
+			credentials: true, // Allow cookies
+		}));
+
+		this.app.use(express.json());
+
+		this.app.use(cookieParser());
+	}
+
+
+
+	// connect routes
+	private createRoutes() {
+		this.app.use("/api/auth", authRouter);
+		this.app.use("/api/user", userRouter);
+		this.app.use("/api/product", productRouter);
+	}
+
+
+	private useErrorMiddleware() {
+		this.app.use(errorMiddleware);
+	}
+
+
+
+	// run server
+	private startServer() {
+		this.app.listen(this.port, () => {
+			console.log("CORS Server PORT:", this.port);
+		})
+	}
+
+}
+
+
+new Server();
