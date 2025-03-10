8a7c3b60ecc5943dd523142539ac3ff1eb4e22d5
joydeep-git <joydeep.accounts@tuta.io>
2025-03-10 10:10:40 +0530

    added router

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -13,7 +13,7 @@ class ProductRouter {
         this.router.get("/owner-all-products/:id", verifyTokenMiddleware, ProductController.ownerAllProducts);
         this.router.post("/update-product/:id/:productId", verifyTokenMiddleware, validProductChecker, ProductController.updateProductDetails);
         this.router.delete("/delete-product/:id/:productId", verifyTokenMiddleware, ProductController.deleteProduct);
-        this.router.get("/get", ProductController.getAllProducts);
+        this.router.get("/get-all-products", ProductController.getAllProducts);
         this.router.post("/edit/:id/:productId", verifyTokenMiddleware, validProductChecker, ProductController.editProperty);
     }
 }
