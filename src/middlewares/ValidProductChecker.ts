369f8b45f53167aa08e4933604da12685395cffa
joydeep-git <joydeep.accounts@tuta.io>
2025-03-10 10:11:20 +0530

    added static statusCode

--- src/middlewares/ValidProductChecker.ts
+++ src/middlewares/ValidProductChecker.ts
@@ -1,24 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
+import { StatusCode } from "../types/index.types";
 
 const validProductChecker = (req: Request, res: Response, next: NextFunction) => {
     
     const { regularPrice, discountAmount, bathrooms, bedrooms } = req.body;
 
     if (Object.entries(req.body).some(([key, value]) => value === "" || value === null || value === undefined)) {
-        return next(new ErrorHandler({ success: false, status: 400, message: "All fields are required!" }));
+        return next(new ErrorHandler({ success: false, status: StatusCode.BAD_REQUEST, message: "All fields are required!" }));
     }
 
     if (isNaN(regularPrice) || isNaN(discountAmount) || isNaN(bathrooms) || isNaN(bedrooms)) {
-        return next(new ErrorHandler({ status: 400, success: false, message: "Invalid numeric values!" }));
+        return next(new ErrorHandler({ status: StatusCode.BAD_REQUEST, success: false, message: "Invalid numeric values!" }));
     }
 
     if (discountAmount < 0 || discountAmount >= regularPrice || regularPrice === 0) {
-        return next(new ErrorHandler({ status: 400, success: false, message: "Invalid Pricing!" }));
+        return next(new ErrorHandler({ status: StatusCode.BAD_REQUEST, success: false, message: "Invalid Pricing!" }));
     }
 
     if (bathrooms <= 0 || bedrooms <= 0) {
-        return next(new ErrorHandler({ status: 400, success: false, message: "Invalid property: Must have at least 1 bathroom & 1 bedroom!" }));
+        return next(new ErrorHandler({ status: StatusCode.BAD_REQUEST, success: false, message: "Invalid property: Must have at least 1 bathroom & 1 bedroom!" }));
     }
 
     next();
